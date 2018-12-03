b597280a2f53a8de4b5a75f1da95ddebe860d20b
galyastoyanova <43786376+galyastoyanova@users.noreply.github.com>
2018-12-03 13:36:44 +0200

    Add files via upload

---
+++ Chapter7/1_/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[20];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * 5;
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
