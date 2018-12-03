26e2ad6ceca346b2d5ae07df6ec8b723948c66b7
galyastoyanova <43786376+galyastoyanova@users.noreply.github.com>
2018-12-03 13:40:06 +0200

    Add files via upload

---
+++ Chapter9/1_/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_
+{
+    class Program
+    {
+            static void ReturnName(string name)
+            {
+                Console.WriteLine("Hello {0}", name);
+            }
+
+            static void Main(string[] args)
+            {
+                Console.Write("Enter name: ");
+                ReturnName(Console.ReadLine());
+            }
+        
+    }
+}
