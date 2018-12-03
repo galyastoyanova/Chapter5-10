409628dab20a529e4c315fddabc2c5c07b379b10
galyastoyanova <43786376+galyastoyanova@users.noreply.github.com>
2018-12-03 13:34:04 +0200

    Add files via upload

---
+++ Chapter6/12_/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int n = Int32.Parse(Console.ReadLine());
+            string binary = Convert.ToString(n, 2);
+            Console.WriteLine("Result is {0}", binary);
+        }
+    }
+}
---
+++ Chapter6/1_/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter first number: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i < length; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
